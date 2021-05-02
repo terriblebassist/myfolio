0a4a64fdb1cc4de485150a75a46b3d29587f8158
terriblebassist <asingh175@gmail.com>
2021-05-02 20:14:59 +0530

    reverted deprecated plugin

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import PrerenderSPAPlugin from 'prerender-spa-plugin'
-import path from 'path'
-
-// const path = require('path')
-// const PrerenderSPAPlugin = require('prerender-spa-plugin')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    new PrerenderSPAPlugin(
-      // Absolute path to compiled SPA
-      path.resolve(__dirname, 'dist'),
-      // List of routes to prerender
-      [ '/'],
-      // {
-      //   // options
-      // }
-    ),
-  ]
+  plugins: [vue()]
 })
