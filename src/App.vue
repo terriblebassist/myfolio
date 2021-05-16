c1d6ebce03c490971ae52db315455ee3f1e17ccb
terriblebassist <asingh175@gmail.com>
2021-05-16 23:04:06 +0530

    logo updated

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,9 @@
 				<div class="flex items-center justify-between">
 					<div>
 						<a class="flex items-center text-gray-800 hover:text-indigo-600" href="#">
-							<svg class="h-6 w-6 sm:h-6 sm:w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-							</svg>
+							<object class="h-6 w-6 sm:h-6 sm:w-6" type="image/svg+xml" data="./src/assets/logo.svg"></object>
 							<span class="mx-3 font-medium text-sm md:text-base">Abhinav Singh</span>
+
 						</a>
 					</div>
 					<div class="flex items-center -mx-2">
